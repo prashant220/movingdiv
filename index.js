b9a5d89d76c2c6836ee1730a688ee234602e9f57
prashant220 <ppoudel624@gmail.com>
2021-04-30 10:47:32 +0545

    second

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function move(){
     position += 10;
     console.log(position)
     box.style.left=position+'px';
-    if(position==450){
+    if(position==460){
         stop()
     }
     
@@ -34,11 +34,49 @@ function move(){
         position2 += 10;
         console.log(position2)
         box2.style.top=position2+'px';
-        if(position2==130){
+        if(position2==180){
             stop2()
         }
         
         }
+
+    
+        
+    window.onload = function(){
+        var canvas = document.getElementById("bounce");
+        var ctx = canvas.getContext("2d");
+        var x = 0;
+        var y = 0;
+        var dx = 1;
+        var dy = -1;
+    
+        draw();
+    
+        function draw(){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.beginPath();
+            ctx.rect(x, y, 20, 10);
+            ctx.fillStyle = "yellow";
+            ctx.fill();
+            ctx.closePath();
+    
+            if(x + dx > canvas.width-10){
+                dx = -dx;
+            }
+            if(x + dx < 0){
+                dx = -dx;
+            }
+            if(y + dy > canvas.height-10){
+                dy = -dy;
+            }
+            if(y + dy < 0){
+                dy = -dy;
+            }
+            x += dx;
+            y += dy;
+        }
+        setInterval(draw, 10)
+    }
         
       
 
